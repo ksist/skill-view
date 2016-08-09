9f57df2b4e2978808cbe168b1ca128fae358ead8
kasai <s-kasai@ncsa.jp>
2016-08-09 14:28:12 +0900

    add career entry

--- js/entry/main.js
+++ js/entry/main.js
@@ -10,19 +10,19 @@ requirejs.config(
   paths:
   //injector:mainReleasePaths
   {
-    "knockout": "../bower_components/knockout/dist/knockout.debug",
-    "jquery": "../bower_components/jquery/dist/jquery",
-    "jqueryui-amd": "../bower_components/jquery-ui/ui",
-    "promise": "../bower_components/es6-promise/promise",
-    "hammerjs": "../bower_components/hammerjs/hammer",
-    "ojdnd": "../bower_components/oraclejet/dist/js/libs/dnd-polyfill/dnd-polyfill-1.0.0",
-    "ojs": "../bower_components/oraclejet/dist/js/libs/oj/debug",
-    "ojL10n": "../bower_components/oraclejet/dist/js/libs/oj/ojL10n",
-    "ojtranslations": "../bower_components/oraclejet/dist/js/libs/oj/resources",
-    "knockout-amd-helpers": "../bower_components/knockout-amd-helpers/build/knockout-amd-helpers",
-    "text": "../bower_components/text/text",
-    "signals": "../bower_components/js-signals/dist/signals",
-    "moment": "../bower_components/moment/moment"
+    "knockout": "../../bower_components/knockout/dist/knockout.debug",
+    "jquery": "../../bower_components/jquery/dist/jquery",
+    "jqueryui-amd": "../../bower_components/jquery-ui/ui",
+    "promise": "../../bower_components/es6-promise/promise",
+    "hammerjs": "../../bower_components/hammerjs/hammer",
+    "ojdnd": "../../bower_components/oraclejet/dist/js/libs/dnd-polyfill/dnd-polyfill-1.0.0",
+    "ojs": "../../bower_components/oraclejet/dist/js/libs/oj/debug",
+    "ojL10n": "../../bower_components/oraclejet/dist/js/libs/oj/ojL10n",
+    "ojtranslations": "../../bower_components/oraclejet/dist/js/libs/oj/resources",
+    "knockout-amd-helpers": "../../bower_components/knockout-amd-helpers/build/knockout-amd-helpers",
+    "text": "../../bower_components/text/text",
+    "signals": "../../bower_components/js-signals/dist/signals",
+    "moment": "../../bower_components/moment/moment"
   }
   //endinjector
   ,
@@ -58,6 +58,7 @@ require(["ojs/ojcore",
             self.titleLabel = ko.observable("スキル可視化");
             self.copyright = ko.observable("Copyright © 2016, NCS&A Co., Ltd.");
             self.params = ko.observable();
+            self.test = ko.observable();
 
             self.menuItemSelect = function(event, ui) {
                 window.location.href = ui.item.children("a")[0].href;
@@ -67,7 +68,7 @@ require(["ojs/ojcore",
             var router = oj.Router.rootInstance;
             router.configure({
                 'search': {label: '社員検索', isDefault: true},
-                'employee': {label:'社員情報',
+                'careerEntry': {label:'社員情報',
                     enter: function() {
                         var childRouter = router.createChildRouter('emp');
                         router.currentState().value = childRouter;
@@ -76,11 +77,7 @@ require(["ojs/ojcore",
                         var childRouter = router.currentState().value;
                         childRouter.dispose();
                     }
-                },
-                'employeeSearch': {label: '社員一覧'},
-                'examSearch': {label: '資格検索'},
-                'educationSearch': {label: '教育検索'},
-                'inventorySearch': {label: 'スキル検索'}
+                }
             })
             self.router = router;
             self.moduleConfig = ko.pureComputed(function () {
@@ -100,48 +97,8 @@ require(["ojs/ojcore",
              * 社員選択時
              */
             self.showEmployee = function(employeeCode, data, event) {
-                history.pushState(null, '', '?root=employee&emp=' + employeeCode);
+                history.pushState(null, '', '?root=careerEntry&emp=' + employeeCode);
                 oj.Router.sync();
-            }
-
-            /**
-             * 社員の絞り込み
-             * dataに登録されている条件で絞り込む
-             */
-            self.showEmployeeList = function(data, event) {
-                self.params = data;
-                history.pushState(null, '', '?root=employeeSearch');
-                oj.Router.sync();
-            }
-
-            /**
-             * 社員検索タブ
-             * optionChangeHandler は変更された時しか呼ばれないため、
-             * 選択されているタブがクリックされた時用（以下３つも同じ）
-             */
-            self.showHome = function() {
-                self.router.go('search');
-            }
-
-            /**
-             * 資格検索タブ
-             */
-            self.showExam = function() {
-                self.router.go('examSearch');
-            }
-            
-            /**
-             * 教育検索タブ
-             */
-            self.showEducation = function() {
-                self.router.go('educationSearch');
-            }
-
-            /**
-             * スキル検索タブ
-             */
-            self.showInventory = function() {
-                self.router.go('inventorySearch');
             }
           
             oj.Router.defaults['urlAdapter'] = new oj.Router.urlParamAdapter();
---
+++ js/entry/viewModels/careerEntry.js
@@ -0,0 +1,349 @@
+define(['ojs/ojcore', 'knockout', 'jquery', 'moment', 'ojs/ojtable',　'ojs/ojdialog', 
+        'ojs/ojrowexpander', 'ojs/ojdatetimepicker', 'ojs/ojselectcombobox',
+        'ojs/ojflattenedtreedatagriddatasource', 'ojs/ojjsontreedatasource', 'ojs/ojknockout-validation'],
+    function (oj, ko, $, moment)
+    {
+        function careerEntryViewModel() {
+            var self = this;
+            self.moment = moment;
+            self.startDate = ko.observable("");
+            self.endDate = ko.observable("");
+            self.industryType = ko.observable("");
+            self.os = ko.observable("");
+            self.language = ko.observable("");
+            self.db = ko.observable("");
+            self.workName = ko.observable("");
+            self.workOutline = ko.observable("");
+            self.projectCount = ko.observable("");
+            self.role = ko.observable([]);
+            self.phases = ko.observableArray([]);
+            self.careers = ko.observableArray([]);
+            self.careerData = ko.observable();
+
+            self.dateConverter = oj.Validation.converterFactory(oj.ConverterFactory.CONVERTER_TYPE_DATETIME)
+                .createConverter({pattern: 'yyyy/MM'});
+            self.tracker = ko.observable();
+
+            self.buttonMode = ko.observable("add");
+
+            // テスト用（実際にはDBから取得する））
+            self.phaseArray = [
+                {value: "1.調査分析・要件定義"},
+                {value: "2.基本設計"},
+                {value: "3.機能設計"},
+                {value: "4.詳細設計"},
+                {value: "5.製造（ｺｰﾃﾞｨﾝｸﾞ）"},
+                {value: "6.単体ﾃｽﾄ"},
+                {value: "7.結合ﾃｽﾄ"},
+                {value: "8.総合ﾃｽﾄ"},
+                {value: "9.受入・運用ﾃｽﾄ"},
+                {value: "10.ｼｽﾃﾑ運用・保守"},
+                {value: "11.DB設計"},
+                {value: "12.DB構築"},
+                {value: "13.ｻｰﾊﾞ設計"},
+                {value: "14.ｻｰﾊﾞ構築"},
+                {value: "15.ｻｰﾊﾞ運用・保守"},
+                {value: "16.NW設計"},
+                {value: "17.NW構築"},
+                {value: "18.NW運用・保守"},
+                {value: "19.品質分析・PMO"},
+                {value: "20.その他"}
+            ]
+            self.roleArray = [
+                {value: ""},
+                {value: "プロジェクトマネージャー"},
+                {value: "プロジェクトリーダー"},
+                {value: "サブリーダー"},
+                {value: "メンバー"}
+            ]
+
+            /**
+             * 初期ロード時に呼ばれる
+             */
+            self.handleActivated = function(info) {
+                var parentRouter = info.valueAccessor().params.ojRouter.parentRouter;
+                self.empRouter = parentRouter.currentState().value;
+
+                self.empRouter.configure(function (stateId) {
+                    var state;
+                    if (stateId) {
+                        var data = stateId.toString();
+                        state = new oj.RouterState(data, {
+                            value: data,
+                            canEnter: function () {
+                                return self.loadData(data);
+                            }
+                        });
+                    }
+                    return state;
+                });
+                return oj.Router.sync();
+            };
+
+            /**
+             * 社員データの読み込み
+             */
+            self.loadData = function (id) {
+                return new Promise(function (resolve, reject) {
+                    var url = "js/test/employee" + id + ".json";
+                    $.getJSON(url).then(function(person) {
+                        self.employee = person;
+                        person.careers.forEach(function (career, i) {
+                            self.careers.push(convertCareerArray(career));
+                        })
+                        var options = [];
+                        self.careerData(new oj.FlattenedTreeTableDataSource(
+                            new oj.FlattenedTreeDataGridDataSource(
+                                new oj.JsonTreeDataSource(self.careers()), options)));
+
+                        resolve(true);
+                    }).fail(function (error) {
+                        console.log('Error: ' + error.message);
+                        resolve(false);
+                    });
+                });
+            };
+
+            /**
+             * 配列から重複しているものを除いた配列を返す
+             * 重複しているかを見る項目はitemで指定する
+             * @return 重複した項目を除いた後の配列
+             */
+            function distinctArray(array, item) {
+                var tempArray = new Array();
+                ko.utils.arrayForEach(array, function(r){
+                    tempArray.push(r[item]);
+                })
+                return ko.utils.arrayGetDistinctValues(tempArray);
+            }
+
+            /**
+             * 追加ボタン
+             */
+            self.addClick = function(data, event) {
+                // エラーチェック
+                var trackerObj = ko.utils.unwrapObservable(self.tracker);
+                if (!this._showComponentValidationErrors(trackerObj)) {
+                    return;
+                }
+
+                // @TODO 日付チェックを実装したらコメントを外す
+                // if (!this._runAppLevelValidation(trackerObj)) {
+                //     return;
+                // }
+
+                // RowExpander で使用している id をリセット
+                self.careers().forEach(function(career, i) {
+                    delete self.careers()[i].attr.id;
+                    delete self.careers()[i].children[0].attr.id;
+                });
+                var career = {};
+                career.industryType = self.industryType();
+                career.startDate = self.startDate();
+                career.endDate = self.endDate();
+                career.workName = self.workName();
+                career.os = self.os();
+                career.language = self.language();
+                career.db = self.db();
+                career.workOutline = self.workOutline();
+                career.projectCount = self.projectCount();
+                career.role = self.role();
+                career.phases = [];
+                self.phases().forEach(function(phase, i) {
+                    career.phases.push({"phaseName": phase})
+                })
+                self.careers.unshift(convertCareerArray(career));
+                var options = [];
+                self.careerData(new oj.FlattenedTreeTableDataSource(
+                    new oj.FlattenedTreeDataGridDataSource(
+                        new oj.JsonTreeDataSource(self.careers()), options)));
+                clearForm();
+            }
+
+            /**
+             * 追加ボタンの表示／非表示切り替え
+             */
+            self.shouldDisableAddButton = function()
+            {
+                var trackerObj = ko.utils.unwrapObservable(self.tracker), 
+                    hasInvalidComponents = trackerObj ? trackerObj["invalidShown"] : false;
+                return  hasInvalidComponents;
+            };
+
+            /**
+             * 削除ボタン
+             */
+            self.delClick = function(data, event) {
+                $("#modalDialog").ojDialog("open");
+                self.rowId = data.id;
+            }
+
+            /**
+             * ダイアログのOKボタン
+             */
+            self.handleOKClose = function(data, event) {
+                $("#modalDialog").ojDialog("close"); 
+                self.careers().forEach(function(career, i) {
+                    if (career.attr.id === self.rowId) {
+                        self.careers.splice(i, 1);
+                    }
+                    // RowExpander で使用している id をリセット
+                    delete self.careers()[i].attr.id;
+                    delete self.careers()[i].children[0].attr.id;
+                });
+                var options = [];
+                self.careerData(new oj.FlattenedTreeTableDataSource(
+                    new oj.FlattenedTreeDataGridDataSource(
+                        new oj.JsonTreeDataSource(self.careers()), options)));
+                clearForm();
+            };
+
+            /**
+             * ダイアログのキャンセルボタン
+             */
+            self.handleCancelClose = function(data, event) {
+                $("#modalDialog").ojDialog("close");
+            };
+            
+            /**
+             * 編集ボタン
+             */
+            self.editClick = function(data, event) {
+                var tempCareer;
+                self.careers().forEach(function(career, i) {
+                    if (career.attr.id === data.id) {
+                        tempCareer = self.careers()[i];
+                    }
+                });
+                self.startDate(tempCareer.attr.startDate);
+                self.endDate(tempCareer.attr.endDate);
+                self.industryType(tempCareer.attr.industryType);
+                self.workName(tempCareer.attr.workName);
+                self.os(tempCareer.attr.os);
+                self.language(tempCareer.attr.language);
+                self.db(tempCareer.attr.db);
+                self.workOutline(tempCareer.children[0].attr.workOutline);
+                self.phases.removeAll();
+                tempCareer.children[0].attr.phases.forEach(function(phase, i) {
+                    self.phases.push(phase.phaseName);
+                });
+                self.role(tempCareer.children[0].attr.role);
+                self.projectCount(tempCareer.children[0].attr.projectCount);
+                self.buttonMode("update");
+                self.rowId = data.id;
+            }
+
+            /**
+             * 更新ボタン
+             */
+            self.updateClick = function(data, event) {
+                // エラーチェック
+                var trackerObj = ko.utils.unwrapObservable(self.tracker);
+                if (!this._showComponentValidationErrors(trackerObj)) {
+                    return;
+                }
+
+                // @TODO 日付チェックを実装したらコメントを外す
+                // if (!this._runAppLevelValidation(trackerObj)) {
+                //     return;
+                // }
+
+                tempCareer = {};
+                tempCareer.industryType = self.industryType();
+                tempCareer.startDate = self.startDate();
+                tempCareer.endDate = self.endDate();
+                tempCareer.workName = self.workName();
+                tempCareer.os = self.os();
+                tempCareer.language = self.language();
+                tempCareer.db = self.db();
+                tempCareer.workOutline = self.workOutline();
+                tempCareer.projectCount = self.projectCount();
+                tempCareer.role = self.role();
+                tempCareer.phases = [];
+                self.phases().forEach(function(phase, i) {
+                    tempCareer.phases.push({"phaseName": phase})
+                })
+                self.careers().forEach(function(career, i) {
+                    if (career.attr.id === self.rowId) {
+                        self.careers()[i] = convertCareerArray(tempCareer);
+                    }
+                });
+                var options = [];
+                self.careerData(new oj.FlattenedTreeTableDataSource(
+                    new oj.FlattenedTreeDataGridDataSource(
+                        new oj.JsonTreeDataSource(self.careers()), options)));
+                clearForm();
+            }
+            
+            /**
+             * 業務経歴データを ojRowExpander に渡すために変換
+             * @return 変換後の配列
+             */
+            function convertCareerArray(career) {
+                    var tempCareer = {"attr": {}, "children": [{"attr": {}}]};
+                    tempCareer.attr.industryType = career.industryType;
+                    tempCareer.attr.startDate = career.startDate;
+                    tempCareer.attr.endDate = career.endDate;
+                    tempCareer.attr.workName = career.workName;
+                    tempCareer.attr.os = career.os;
+                    tempCareer.attr.language = career.language;
+                    tempCareer.attr.db = career.db;
+                    tempCareer.children[0].attr.workOutline = career.workOutline;
+                    tempCareer.children[0].attr.projectCount = career.projectCount;
+                    tempCareer.children[0].attr.role = career.role;
+                    tempCareer.children[0].attr.phases = career.phases;
+                return tempCareer;
+            }
+
+            /**
+             * フォームの入力をクリアする
+             */
+            function clearForm() {
+                self.industryType("");
+                self.startDate("");
+                self.endDate("");
+                self.workName("");
+                self.os("");
+                self.language("");
+                self.db("");
+                self.workOutline("");
+                self.projectCount("");
+                self.role("");
+                self.phases([]);
+                self.buttonMode("add");                
+            }
+
+            self._showComponentValidationErrors = function (trackerObj)
+            {
+                trackerObj.showMessages();
+                if (trackerObj.focusOnFirstInvalid())
+                    return false;
+
+                return true;
+            };
+
+            // @TODO 日付チェックを実装したらコメントを外す
+            // self._runAppLevelValidation = function (trackerObj)
+            // {
+            //     var valid = true;
+
+            //     // Validate email address and phoneNumber against the contactPref 
+            //     valid = (this._validateObservable(this.emailAddress,
+            //                                     this.emailAddrCRValidator,
+            //                                     this.emailAddressMessages) &&
+            //             this._validateObservable(this.phoneNumber,
+            //                                     this.phoneNumCRValidator,
+            //                                     this.phoneNumberMessages));                                  
+
+            //     if (!valid)
+            //     {
+            //     trackerObj.focusOnFirstInvalid();
+            //     return false;
+            //     }
+
+            //     return true;
+            // };
+        }
+        return careerEntryViewModel;
+    }
+)
