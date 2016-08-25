535188f16dc70be903e6b4613edc6e96c19e74ed
kasai <s-kasai@ncsa.jp>
2016-08-25 11:25:22 +0900

    change inventory list URL

--- js/viewModels/employeeList.js
+++ js/viewModels/employeeList.js
@@ -14,14 +14,15 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojtable'],
                 // 呼び出し元から渡されたパラメータの種類によってデータの取得元を変更
                 if (self.condition.examName) {
                     // url = "js/test/employee" + self.condition.examName + ".json";
-                    var url = 'http://172.16.9.99/rest/employees/exam/' + self.condition.examName;
+                    url = 'http://172.16.9.99/rest/employees/exam/' + self.condition.examName;
                     self.searchCondition = '資格名： ' + self.condition.examName;
                 } else if (self.condition.educationName) {
                     // url = "js/test/employeeedu" + self.condition.educationCode + ".json";
-                    var url = 'http://172.16.9.99/rest/employees/education/' + self.condition.educationName;
+                    url = 'http://172.16.9.99/rest/employees/education/' + self.condition.educationName;
                     self.searchCondition = '教育名： ' + self.condition.educationName;
                 } else if (self.condition.inventoryCode) {
-                    url = "js/test/employeeskill" + self.condition.inventoryCode + "level" + self.condition.level + ".json";
+                    // url = "js/test/employeeskill" + self.condition.inventoryCode + "level" + self.condition.level + ".json";
+                    url = 'http://172.16.9.99/rest/employees/inventory/' + self.condition.inventoryCode + '/level/' + self.condition.level;
                     self.searchCondition = 'スキル： ' + self.condition.shoName + ' レベル' + self.condition.dispLevel;
                 }
 
